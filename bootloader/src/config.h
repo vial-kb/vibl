2cea8977f2d5f3e5632bfd0638a055c97ed27e97
Ilya Zhuravlev <whatever@xyz.is>
2020-12-26 21:26:22 -0500

    add vial_test target

--- bootloader/src/config.h
+++ bootloader/src/config.h
@@ -5,8 +5,15 @@
 
 #define RTC_BOOTLOADER_FLAG 0x424C
 
-#ifdef TARGET_GENERIC
+#if defined(TARGET_GENERIC)
 #define VIAL_KEYBOARD_UID {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}
+/* setup for bluepill */
+#define BL_OUTPUT_BANK GPIOB
+#define BL_OUTPUT_PIN 12
+#define BL_INPUT_BANK GPIOB
+#define BL_INPUT_PIN 13
+#elif defined(TARGET_VIAL_TEST)
+#define VIAL_KEYBOARD_UID {0x6F, 0xC5, 0xEE, 0x60, 0x90, 0x92, 0x53, 0xA4}
 /* setup for bluepill */
 #define BL_OUTPUT_BANK GPIOB
 #define BL_OUTPUT_PIN 12
