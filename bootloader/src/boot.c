17948035e3eb17d64b7a14e4c55c1bfd6e404807
Ilya Zhuravlev <whatever@xyz.is>
2021-01-01 04:18:28 -0500

    bootloader/boot: some more gpio related cleanup

--- bootloader/src/boot.c
+++ bootloader/src/boot.c
@@ -5,26 +5,6 @@
 
 #include "config.h"
 
-#define SET_REG(addr,val) do { *(volatile uint32_t*)(addr)=val; } while(0)
-#define GET_REG(addr)     (*(volatile uint32_t*)(addr))
-#define GPIO_BSRR(port) ((uint32_t)port+0x10)
-#define CR_INPUT_PU_PD      0x08
-#define CR_OUTPUT_PP        0x01
-#define CR_SHITF(pin) ((pin - 8*(pin>7))<<2)
-#define GPIO_CR(port,pin) ((uint32_t)port + (0x04*(pin>7)))
-
-// Used to create the control register masking pattern, when setting control register mode.
-static unsigned int crMask(int pin)
-{
-    unsigned int mask;
-    if (pin>=8)
-    {
-        pin-=8;
-    }
-    mask = 0x0F << (pin<<2);
-    return ~mask;
-}
-
 int checkUserCode(void) {
     uint32_t sp = *(volatile uint32_t *) USER_PROGRAM;
 
@@ -69,14 +49,33 @@ int checkKbMatrix(void) {
     return !!(BL_INPUT_BANK->IDR & (1U << BL_INPUT_PIN));
 }
 
+#define CR_INPUT_PU_PD      0x08
+#define CR_OUTPUT_PP        0x01
+#define CR_SHITF(pin) ((pin - 8*(pin>7))<<2)
+#define GPIO_CR(port,pin) (__IO uint32_t*)((uint32_t)port + (0x04*(pin>7)))
+
+// Used to create the control register masking pattern, when setting control register mode.
+static uint32_t crMask(uint32_t pin)
+{
+    uint32_t mask;
+    if (pin >= 8)
+        pin -= 8;
+    mask = 0x0F << (pin<<2);
+    return ~mask;
+}
+
 void setupGPIO(void) {
+    uint32_t cr;
+
     /* Enable All GPIO channels (A to G) */
     RCC->APB2ENR |= 0b111111100;
 
     /* Set (BL_INPUT_BANK,BL_INPUT_PIN) as pulldown input */
-    SET_REG(GPIO_CR(BL_INPUT_BANK,BL_INPUT_PIN),(GET_REG(GPIO_CR(BL_INPUT_BANK,BL_INPUT_PIN)) & crMask(BL_INPUT_PIN)) | CR_INPUT_PU_PD << CR_SHITF(BL_INPUT_PIN));
+    cr = *GPIO_CR(BL_INPUT_BANK,BL_INPUT_PIN) & crMask(BL_INPUT_PIN);
+    *GPIO_CR(BL_INPUT_BANK,BL_INPUT_PIN) = cr | CR_INPUT_PU_PD << CR_SHITF(BL_INPUT_PIN);
     BL_INPUT_BANK->BRR = (1U << BL_INPUT_PIN);
 
     /* Set (BL_OUTPUT_BANK,BL_OUTPUT_PIN) as output */
-    SET_REG(GPIO_CR(BL_OUTPUT_BANK,BL_OUTPUT_PIN),(GET_REG(GPIO_CR(BL_OUTPUT_BANK,BL_OUTPUT_PIN)) & crMask(BL_OUTPUT_PIN)) | CR_OUTPUT_PP << CR_SHITF(BL_OUTPUT_PIN));
+    cr = *GPIO_CR(BL_OUTPUT_BANK,BL_OUTPUT_PIN) & crMask(BL_OUTPUT_PIN);
+    *GPIO_CR(BL_INPUT_BANK,BL_INPUT_PIN) = cr | CR_OUTPUT_PP << CR_SHITF(BL_OUTPUT_PIN);
 }
